0ea38a3101e2b151c320e23f5526dd144d6cc112
AakibAlam <aakibalam20@iitk.ac.in>
2024-02-19 22:34:16 +0530

    testing gemini integration

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from resume.views import index
+from resume.views import index, generate
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('submit/', generate, name='generate')
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,4 +1,35 @@
+import os
 from django.shortcuts import render
+from django.http import JsonResponse
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def index(request):
     return render(request, 'index.html')
+
+
+
+def generate(request):
+
+    if request.method != 'POST':
+        return JsonResponse({'output_text': 'not post method'})
+    
+    if 'input_text' not in request.POST:
+        return JsonResponse({'output_text': 'no input_text key in request.POST'})
+
+    GOOGLE_API_KEY = os.getenv('API_KEY0')
+    genai.configure(api_key=GOOGLE_API_KEY)
+    generation_config = {
+        "temperature": 0,
+        "top_p": 1,
+        "top_k": 45,
+        "max_output_tokens": 4096,
+    }
+    model = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config)
+    input_text = request.POST.get('input_text')
+    
+    output_text = model.generate_content(input_text)
+    output_text.resolve()
+    return JsonResponse({'output_text': output_text.text})
