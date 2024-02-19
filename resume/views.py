4db78c9a96a2f0856c7d92bd134fedca11e85bce
AakibAlam <aakibalam20@iitk.ac.in>
2024-02-19 22:42:54 +0530

    API_KEY test

--- resume/views.py
+++ resume/views.py
@@ -13,23 +13,25 @@ def index(request):
 
 def generate(request):
 
-    if request.method != 'POST':
-        return JsonResponse({'output_text': 'not post method'})
-    
-    if 'input_text' not in request.POST:
-        return JsonResponse({'output_text': 'no input_text key in request.POST'})
+    return JsonResponse({'API_KEY': os.getenv('API_KEY0')})
 
-    GOOGLE_API_KEY = os.getenv('API_KEY0')
-    genai.configure(api_key=GOOGLE_API_KEY)
-    generation_config = {
-        "temperature": 0,
-        "top_p": 1,
-        "top_k": 45,
-        "max_output_tokens": 4096,
-    }
-    model = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config)
-    input_text = request.POST.get('input_text')
+    # if request.method != 'POST':
+    #     return JsonResponse({'output_text': 'not post method'})
     
-    output_text = model.generate_content(input_text)
-    output_text.resolve()
-    return JsonResponse({'output_text': output_text.text})
+    # if 'input_text' not in request.POST:
+    #     return JsonResponse({'output_text': 'no input_text key in request.POST'})
+
+    # GOOGLE_API_KEY = os.getenv('API_KEY0')
+    # genai.configure(api_key=GOOGLE_API_KEY)
+    # generation_config = {
+    #     "temperature": 0,
+    #     "top_p": 1,
+    #     "top_k": 45,
+    #     "max_output_tokens": 4096,
+    # }
+    # model = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config)
+    # input_text = request.POST.get('input_text')
+    
+    # output_text = model.generate_content(input_text)
+    # output_text.resolve()
+    # return JsonResponse({'output_text': output_text.text})
